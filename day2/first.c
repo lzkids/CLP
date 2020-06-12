767d5661097d8ff8a352cbb259f2cae010337a87
lzkids <kidsde1314@gmail.com>
2020-06-12 18:23:23 +0800

    添加内容与检测代码文件

---
+++ day2/first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+    int num ;
+    num = 1;
+
+    printf ("I am simple ");   // 打印输出
+    printf ("computer. \n");
+    printf ("My favorite number is %d because it is first .\n", num);
+
+    return 0; 
+}
